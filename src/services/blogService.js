265a5b55bae92b1baf4b3087a2f1657011ef34bb
Matheus Nascimento <matheus.l.nascimento@globalhitss.com.br>
2025-06-12 00:04:54 -0300

    Fix language

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 async function getPosts() {
-  const response = await axios.get(`https://newsapi.org/v2/everything?q=computer&from=2025-05-11&sortBy=publishedAt&apiKey=${process.env.API_KEY}`);
+  const response = await axios.get(`https://newsapi.org/v2/everything?q=tecnologia&language=pt&from=2025-05-11&sortBy=publishedAt&apiKey=${process.env.API_KEY}`);
   return response.data.articles;
 }
 
